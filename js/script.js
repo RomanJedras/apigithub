d26e6d14a0505dd418a998d1406369416ff8b612
Roman Jędras <romanjedras@o2.pl>
2019-06-22 13:26:41 +0200

    Search user by Github

---
+++ js/script.js
@@ -0,0 +1,82 @@
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      searchText: '',
+      users: []
+    };
+  }
+  
+  onChangeHandle(event) {
+    this.setState({searchText: event.target.value});
+  }
+  
+  onSubmit(event) {
+    event.preventDefault();
+    const {searchText} = this.state;
+    const url = `https://api.github.com/search/users?q=${searchText}`;
+    fetch(url)
+    .then(response => response.json())
+    .then(responseJson => this.setState({users: responseJson.items}));
+    console.log(this.state.users);
+  }
+  
+  render() {
+    return (
+      <div className={'mt-2 mb-3 d-flex justify-content-center'}>
+        <form onSubmit={event => this.onSubmit(event)}>
+          <label htmlFor="searchText">Search by user name</label>
+          <input
+            type="text"
+            id="searchText"
+            onChange={event => this.onChangeHandle(event)}
+            value={this.state.searchText}/>
+        </form>
+        <UsersList users={this.state.users}/>
+      </div>
+    );
+  }
+}
+
+class UsersList extends React.Component {
+  get users() {
+    return this.props.users.map(user => <User key={user.id} user={user}/>);
+  }
+  
+  render() {
+    return (
+      <div className={'row user-list'}>
+        {this.users}
+      </div>
+    );
+  }
+}
+
+class User extends React.Component {
+  render() {
+    return (
+      <div className={'col-xs-12 col-md-4 mb-3 mt-2 justify-content-between'}>
+        <img src={this.props.user.avatar_url} style={{maxWidth: '100px'}}/>
+        <a href={this.props.user.html_url} target="_blank">{this.props.user.login}</a>
+      </div>
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+ReactDOM.render(
+<App />,
+  document.getElementById('root')
+);
